0daa8244cc411d1fc31ffc834f24e54df1a76b67
Jose Romary <joseromarybrandao@gmail.com>
2023-04-13 00:44:32 -0300

    update: contatenando taks inseridas

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -2,11 +2,13 @@ interface ButtonProps {
   name: string;
   type: "button" | "submit" | "reset";
   status?: "concluido" | "andamento" | "pendente";
+  onClick?: () => void;
 }
 
-export default function Button({ name, type, status }: ButtonProps) {
+export default function Button({ name, type, status, onClick }: ButtonProps) {
   return (
     <button
+      onClick={onClick}
       type={type}
       className={`w-full h-8 text-white font-bold rounded-lg ${status === "concluido" ? " bg-green-400" : status === "andamento" ? "bg-yellow-400" : status === "pendente" ? "bg-red-400" : 'bg-sky-500'}`}
     >
--- src/Components/TaskCreator/index.tsx
+++ src/Components/TaskCreator/index.tsx
@@ -1,30 +1,103 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from 'uuid';
+import * as zod from 'zod';
 import Button from "../Button";
 
+interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+  created_at: string;
+}
+
+
+const newTaskValidationSchema = zod.object({
+  id: zod.string(),
+  title: zod.string().min(3, 'Título precisa ter no mínimo 3 caracteres'),
+  description: zod.string(),
+  status: zod.string(),
+  created_at: zod.string()
+})
+
+type newPostFormData = zod.infer<typeof newTaskValidationSchema>
+
 export default function TaskCreator() {
+  const [task, setTask] = useState<Task[]>([])
+
+  const handleCreateNewTask = (data: newPostFormData) => {
+    data.created_at = moment().locale('pt-br').format('DD/MM/YYYY')
+    data.id = uuidv4()
+
+    setTask([...task, data]);
+
+  }
+  console.log("taks inserida", task)
+
+  const { register, handleSubmit, formState, reset } = useForm({
+    resolver: zodResolver(newTaskValidationSchema),
+    defaultValues: {
+      id: '',
+      title: '',
+      description: '',
+      status: '',
+      created_at: '',
+    },
+  })
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset({
+        title: '',
+        description: '',
+        status: '',
+        id: '',
+        created_at: '',
+      });
+    }
+  }, [formState, reset]);
+
+
   return (
     <div className="w-full min-w-[275px] h-auto mt-8 p-4 border-1 rounded-lg border-solid border-2 border-sky-600">
 
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewTask)}>
+
         <div className="flex flex-col">
           <span className="text-lg" >Criar nova tarefa</span>
 
           <label className="mt-4 text-gray-700 text-sm font-bold mb-2">
             Título
           </label>
-          <input className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 " />
+          <input
+            className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 "
+            {...register('title')}
+          />
 
           <label className="mt-4 text-gray-700 text-sm font-bold mb-2">
             Descrição
           </label>
-          <textarea className="appearance-none resize-none border rounded-lg w-full py-2 px-3 text-gray-700" />
+          <textarea
+            className="appearance-none resize-none border rounded-lg w-full py-2 px-3 text-gray-700"
+            {...register('description')}
+          />
 
-          <div className="mt-4">
-            <select name="statusTask" className="w-full h-10 rounded-lg text-gray-700 border bg-inherit">
-              <option value="pendente">Pendente</option>
-              <option value="andamento">Em andamento</option>
-              <option value="concluido">Concluído</option>
-            </select>
-          </div>
+          <label className="mt-4 text-gray-700 text-sm font-bold mb-2">
+            Status
+          </label>
+          <select
+            className="w-full h-10 rounded-lg text-gray-700 border bg-inherit"
+            {...register('status')}
+          >
+            <option value="andamento">Em andamento</option>
+            <option value="pendente">Pendente</option>
+            <option value="concluido">Concluído</option>
+          </select>
+
 
           <div className="w-full flex items-center justify-center mt-4">
             <Button
@@ -32,13 +105,6 @@ export default function TaskCreator() {
               type="submit"
             />
           </div>
-
-          {/* <button
-            type="submit"
-            className=" w-40 h-8 mt-4 text-white font-bold rounded-lg bg-sky-600"
-          >
-            Criar!
-          </button> */}
 
         </div>
       </form>
